ebda9cbe652735df3e0ad68f8be5bdfaf54b2e24
vkhomchak <vkhomchak@llnw.com>
2015-02-20 15:29:12 +0200

    Added manaul tests methods

--- api.py
+++ api.py
@@ -46,6 +46,38 @@ def add_results(project, sprint):
     return make_response('', 200)
 
 
+@api.route('/manual/<project>', methods=['POST'])
+def add_manual_tests(project):
+    db = get_db(project)
+
+    tests = json.loads(request.get_data())
+
+    for test in tests:
+        db.upsert_test(component=test['component'],
+                       suite=test['suite'],
+                       test_id=test['test_id'],
+                       **test['other_attributes'])
+
+    return make_response('', 200)
+
+
+@api.route('/manual/results/<project>/<sprint>', methods=['POST'])
+def add_manual_results(project, sprint):
+    db = get_db(project)
+
+    results = json.loads(request.get_data())
+
+    for result in results:
+        db.upsert_test_result(sprint=sprint,
+                              component=result['component'],
+                              suite=result['suite'],
+                              test_id=result['test_id'],
+                              **dict(result['result_attributes'].items() +
+                                     result['other_attributes'].items()))
+
+    return make_response('', 200)
+
+
 if __name__ == '__main__':
 
     api.run(host='0.0.0.0', port=5001, debug=True)
